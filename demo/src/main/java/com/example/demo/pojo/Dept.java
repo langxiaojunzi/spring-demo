9b41907ab96099c084cc705a5c3536d186974b83
顾展 <guzhan106@126.com>
2019-04-11 14:04:10 +0800

    lombok

--- demo/src/main/java/com/example/demo/pojo/Dept.java
+++ demo/src/main/java/com/example/demo/pojo/Dept.java
@@ -1,46 +1,22 @@
 package com.example.demo.pojo;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * <p>create at 2019/4/10</p>
  *
  * @author guzhan
  * @since 1.0.0
  */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Dept {
     private Integer no;
     private String name;
     private Integer number;
-
-    public Integer getNo() {
-        return no;
-    }
-
-    public void setNo(Integer no) {
-        this.no = no;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    @Override
-    public String toString() {
-        return "Dept{" +
-                "no=" + no +
-                ", name='" + name + '\'' +
-                ", number=" + number +
-                '}';
-    }
 }

8572f834e7738d561fd292301a41dc7c3f0d22c5
顾展 <guzhan106@126.com>
2019-04-24 11:40:15 +0800

    Enum

--- pattern/src/main/java/com/example/pattern/singleton/Singleton.java
+++ pattern/src/main/java/com/example/pattern/singleton/Singleton.java
@@ -32,6 +32,7 @@ public class Singleton implements Serializable{
     }
 
     // 看这里,在反序列化的时候，JVM 会自动调用 readResolve() 这个方法，我们可以在这个方法中替换掉从流中反序列化回来的对象。
+    // enum 类自带这种特殊光环，不用写 readResolve() 方法就可以自动防止反序列化方式对单例的破坏。
     public Object readResolve() throws ObjectStreamException {
         return INSTANCE;
     }

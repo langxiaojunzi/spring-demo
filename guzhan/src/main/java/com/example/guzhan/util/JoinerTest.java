87871f2d79e914cd21e4566766f285e86666fcf3
guzhan <guzhan106@126.com>
2019-10-15 09:27:37 +0800

    add guava

---
+++ guzhan/src/main/java/com/example/guzhan/util/JoinerTest.java
@@ -0,0 +1,14 @@
+package com.example.guzhan.util;
+
+import com.google.common.base.Joiner;
+
+/**
+ * <p>created at 2019/10/14</p>
+ *
+ * @author guzhan
+ * @since 1.0.0
+ */
+public class JoinerTest {
+    //    private static final JoinerTest JOINER_TEST = JoinerTest.on(",").skipNulls();
+    private static final Joiner joiner = Joiner.on(",").skipNulls();
+}
